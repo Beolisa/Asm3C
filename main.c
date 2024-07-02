44570b6b41830633e3791e47c347877248ab7c1b
Beolisa <beolisachau@gmail.com>
2024-07-02 10:51:58 +0700

    init var commit

--- main.c
+++ main.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define MAX_ROW 9
+#define MAX_COLUMN 9
+#define MAX 100
+
+typedef struct POINT
+{
+    int row;    //x coordinate
+    int column; //y coordinate
+    int value;  //0 or 1 value of a node
+    bool isVisited;
+    struct POINT* prev; //Pointer points to the previous node
+} point_t;
+
+point_t matrix[MAX_ROW][MAX_COLUMN];
+
+//Setup Queue
+typedef struct
+{
+    point_t data[MAX]; //Lưu trữ các node
+    int front;  //Chỉ số đầu của hàng đợi
+    int rear;   //Chỉ số cuối của hàng đợi
+} Queue;
+
 int main()
 {
     
